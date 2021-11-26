110896f31775c45707f447cea266af275a5a0474
Siddhesh <siddheshchikane17@gmail.com>
2021-11-27 00:56:51 +0530

    file committed again

--- C#/OOAD/Design Patterns/TictactoeSln/TictactoeCore/Model/Player.cs
+++ C#/OOAD/Design Patterns/TictactoeSln/TictactoeCore/Model/Player.cs
@@ -8,6 +8,20 @@ namespace TictactoeCore.Model
 {
     class Player
     {
-        
+        private string _name;
+        private MarkType _mark;
+        public Player(string name, MarkType mark)
+        {
+            this._mark = mark;
+            this._name = name;
+        }
+        public string Name
+        {
+            get { return _name; }
+        }
+        public MarkType Mark
+        {
+            get { return _mark; }
+        }
     }
 }
